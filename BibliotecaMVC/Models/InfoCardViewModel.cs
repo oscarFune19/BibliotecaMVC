232cbe6d9b00d916cefea76e24f70831f6d7296f
funes <funesoscar619@gmail.com>
2026-08-24 16:08:15 -0600

    Implementación Semana 5 - Flexbox, Grid, responsive y componentes reutilizables

---
+++ BibliotecaMVC/Models/InfoCardViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliotecaMVC.Models
+{
+    public class InfoCardViewModel
+    {
+        public string Icono { get; set; } = string.Empty;
+        public string Titulo { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+    }
+}
